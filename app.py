c4785fb4d374fe5402de3c4fd2736e7fca602322
Sulaymonova Fozila Mo'minjon qizi <fozila.m.sulaymonova@artelelectronics.com>
2025-04-18 11:30:25 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+st.title("Rasmdagi insonni aniqlovchi model")
+
+file = st.file_uploader('Rasm yuklash', type= ['jpeg', 'png', 'gif', 'svg'])
+if file:
+    st.image(file)
+    img = PILImage.create(file)
+    model = load_learner('humantype_model.pkl')
+    
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}%')
+    fig= px.bar(x=probs*100, y = model.dls.vocab)
+    st.plotly_chart(fig) 
